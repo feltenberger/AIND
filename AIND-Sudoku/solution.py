8d8c472bc3f0af670e83cf0551ae50a4db75e525
Seinberg <seinberg@gmail.com>
2017-06-29 20:49:24 -0400

    committing solution

--- AIND-Sudoku/solution.py
+++ AIND-Sudoku/solution.py
@@ -1,4 +1,27 @@
 assignments = []
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+
+    # taken from course notes :)
+    cross_prod = [x+y for x in A for y in B]
+
+    return cross_prod
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+# Assignment addition: diagonals
+diagonal_units = [[x+y for x, y in zip(rows, cols)], [x+y for x, y in zip(rows, cols[::-1])]]
+# Create a list of all units of the puzzle
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
 
 def assign_value(values, box, value):
     """
@@ -25,11 +48,36 @@ def naked_twins(values):
     """
 
     # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
+    naked_twin_map = {}
+    for unit in unitlist:
+        # identify naked twin pairs
+        box_to_twins_map = {}
+        for box in unit:
+            # for boxes with only 2 possible values, populate the values
+            if len(values[box]) == 2:
+                if not values[box] in box_to_twins_map:
+                    box_to_twins_map[values[box]] = [box]
+                else:
+                    box_to_twins_map[values[box]].append(box)
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+        # TODO: use items() and don't keep accessing map
+        for key in box_to_twins_map:
+            # Condition for the candidate to be a naked twin pair
+            if len(box_to_twins_map[key]) == 2:
+                if not key in naked_twin_map:
+                    naked_twin_map[key] = [unit]
+                else:
+                    naked_twin_map[key].append(unit)
+
+    # Eliminate the naked twins as possibilities for their peers
+    for key in naked_twin_map:
+        for unit in naked_twin_map[key]:
+            for box in unit:
+                if values[box] != key:
+                    assign_value(values, box, values[box].replace(key[0], ''))
+                    assign_value(values, box, values[box].replace(key[1], ''))
+
+    return values
 
 def grid_values(grid):
     """
@@ -41,7 +89,14 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    grid_map = {}
+    for box, value in zip(boxes, grid):
+        if value == '.':
+            value = '123456789'
+        grid_map[box] = value
+
+    return grid_map
+
 
 def display(values):
     """
@@ -49,19 +104,73 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
+
 
 def eliminate(values):
-    pass
+    for box, value in values.items():
+        if len(value) == 1:
+            box_peers = peers[box]
+            for peer in box_peers:
+                peer_values = values[peer]
+                assign_value(values, peer, peer_values.replace(value, ''))
+                #values[peer] = peer_values.replace(value, '')
+    return values
+
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                #values[dplaces[0]] = digit
+                assign_value(values, dplaces[0], digit)
+    return values
+
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Naked Twins Strategy
+        values = naked_twins(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
 
 def search(values):
-    pass
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -72,6 +181,10 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = grid_values(grid)
+    values = search(values)
+    return values
+
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
